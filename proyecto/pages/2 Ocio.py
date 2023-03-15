85d900073aa2a89ebdba0fe63ddda77c607bb513
paulabnbh <paula.bendito@hotmail.com>
2023-03-15 20:36:41 +0100

    15

--- proyecto/pages/2 Ocio.py
+++ proyecto/pages/2 Ocio.py
@@ -12,18 +12,18 @@ st.set_page_config(page_title='DisfrutaMadrid • Ocio', page_icon="☀️", lay
 st.title('¿Qué hacemos?')
 ocio = pd.read_csv('csv/planes-def.csv')
 
-barrio, puntuacion, tipo_plan = st.columns(3)
+tipo_plan, puntuacion = st.columns(2)
 
-with barrio:
-    sel1 = st.selectbox('Elige un barrio', ocio.barrio.unique())
-
-with puntuacion:
-    sel_min, sel_max = st.multiselect('Elige puntuación', 
-                                      options = [i for i in range(-1, ocio.puntuacion.max()+1)],
-                                      value = [-1, ocio.puntuacion.max()])
 
 with tipo_plan:
-    sel3 = st.multiselect('Elige categoría', ocio.columns[7::])
+    sel2 = st.selectbox('Elige categoría', ocio.columns[9::])
 
-ocio_filtered = ocio[(ocio.barrio == sel1)] #& (ocio.puntuacion == sel2) & (ocio[sel3] == 1)]
+with puntuacion:
+    sel1= st.selectbox('Elige puntuación',
+                        ocio.puntuacion.unique())
+
+
+
+ocio_filtered = ocio[(ocio.puntuacion == sel1) & (ocio[sel2] == 1)]
 ocio_def = ocio_filtered[['nombre', 'ubicacion', 'barrio', 'puntuacion']]
+ocio_def
--- proyecto/pages/3 Hoteles.py
+++ proyecto/pages/3 Hoteles.py
@@ -8,3 +8,34 @@ import io
 import datetime
 
 st.set_page_config(page_title='DisfrutaMadrid • Hoteles', page_icon="🏨", layout="wide")
+
+
+st.title('¿Dónde nos alojamos?')
+hoteles = pd.read_csv('csv/hoteles-def.csv')
+
+barrio, precio, puntuacion, servicios = st.columns(4)
+
+with barrio:
+    datos = hoteles.barrio.unique().tolist()
+    sel1 = st.multiselect('Elige barrio', datos, default = ['Cortes', 'Sol', 'Recoletos'])
+
+with precio:
+    selmin, selmax = st.select_slider('Elige rango de precio',
+                            options=[i for i in range(0, hoteles.precio.max()+1)],
+                            value=[0, hoteles.precio.max()])   
+st.write(selmin)
+
+with puntuacion:
+    sel3 = st.multiselect('Elige puntuación', hoteles.puntuacion.unique().tolist(), default = ['5.0'])
+
+with servicios:
+    cols = hoteles.columns[10::].tolist()
+    sel4 = st.multiselect('Elige servicios', cols, default = ['Aparcamiento', 'Servicio de lavandería', 'Desayuno bufé'])
+
+
+
+hoteles_filtered = hoteles[(hoteles['barrio'].isin(sel1)) & (hoteles['puntuacion'].isin(sel3)) & (hoteles.precio > selmin) & (hoteles.precio < selmax)] # & hoteles.precio == selmax)] #& (hoteles[sel4] == 1)]
+
+hoteles_def = hoteles_filtered[['nombre', 'barrio', 'descripcion', 'precio', 'puntuacion']]
+
+st.dataframe(hoteles_def)
